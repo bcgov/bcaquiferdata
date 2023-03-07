aac9ac21ccd7c56661643ef42eca7711d902337f
Steffi LaZerte <steffi@steffi.ca>
2023-03-07 14:31:16 -0600

    Creating test data

--- data-raw/test_data.R
+++ data-raw/test_data.R
@@ -1,6 +1,12 @@
 data_update(download = FALSE)
 
-mill <- st_read("misc/data/MillBayWatershed.shp")
+mill <- sf::st_read("misc/data/MillBayWatershed.shp")
 mill_lidar <- lidar_region(mill)
+
+mill_wells <- wells_subset(mill)
+
 mill_wells <- wells_elev(mill, mill_lidar)
+
+y <- wells_yield(mill_wells)
+
 readr::write_rds(mill_wells, "mills.rds")
