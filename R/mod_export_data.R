7092f848bf360ca340e252ad717b10eed6b9a541
Steffi LaZerte <steffi@steffi.ca>
2023-09-18 16:31:10 -0500

    Add leapfrog and surfer exports to Shiny app

--- R/mod_export_data.R
+++ R/mod_export_data.R
@@ -29,6 +29,8 @@ ui_export_data <- function(id) {
                        "Choose output folder",
                        "Choose where to save files")
       ),
+
+      # UI - Strater -------------
       nav_panel(
         title = "Strater",
         actionButton(ns("export_strater"), "Export", width = 150),
@@ -46,6 +48,8 @@ ui_export_data <- function(id) {
             title = h5("Wells (", textOutput(ns("strater_f3"), container = code), ")"),
             DT::dataTableOutput(ns("table_strater_f3"))))
       ),
+
+      # UI - Voxler -------------
       nav_panel(title = "Voxler",
                 actionButton(ns("export_voxler"), "Export", width = 150),
                 textOutput(ns("feedback_voxler")),
@@ -53,6 +57,7 @@ ui_export_data <- function(id) {
                 h3("Voxler file (", textOutput(ns("voxler_f1"), container = code), ")"),
                 DT::dataTableOutput(ns("table_voxler_f1"))),
 
+      # UI - ArcHydro -------------
       nav_panel(
         title = "ArcHydro",
         actionButton(ns("export_archydro"), "Export", width = 150),
@@ -70,7 +75,35 @@ ui_export_data <- function(id) {
             title = h5("BH (lithology index ", textOutput(ns("archydro_f3"), container = code), ")"),
             DT::dataTableOutput(ns("table_archydro_f3")))
         )
+      ),
+
+      # UI - Leapfrog -------------
+      nav_panel(
+        title = "Leapfrog",
+        actionButton(ns("export_leapfrog"), "Export", width = 150),
+        textOutput(ns("feedback_leapfrog")),
+        navset_card_tab(
+          nav_panel(
+            title = h5("Collars (", textOutput(ns("leapfrog_f1"), container = code), ")"),
+            DT::dataTableOutput(ns("table_leapfrog_f1"))),
+
+          nav_panel(
+            title = h5("Intervals (", textOutput(ns("leapfrog_f2"), container = code), ")"),
+            DT::dataTableOutput(ns("table_leapfrog_f2")))
+        )
+      ),
+
+      # UI - Surfer -------------
+      nav_panel(
+        title = "Surfer",
+        actionButton(ns("export_surfer"), "Export", width = 150),
+        textOutput(ns("feedback_surfer")),
+
+        h3("Surfer file (", textOutput(ns("surfer_f1"), container = code), ")"),
+        DT::dataTableOutput(ns("table_surfer_f1"))
       )
+
+
     )
   )
 }
@@ -79,7 +112,7 @@ server_export_data <- function(id, wells) {
 
   moduleServer(id, function(input, output, session) {
 
-    # Setup
+    # Setup ----------------------
     volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
     feedback <- reactiveValues(strater = "", voxler = "", archydro = "")
 
@@ -106,7 +139,7 @@ server_export_data <- function(id, wells) {
     }
 
 
-    # Setup Directory and File IDs
+    # Setup Directory and File IDs ------------------
     export_id <- reactive(janitor::make_clean_names(input$export_id))
 
     shinyDirChoose(input, "choose_export_dir", session = session,
@@ -127,8 +160,10 @@ server_export_data <- function(id, wells) {
     files <- reactive({
       paste0(export_id(),
              c("_lith.csv", "_collars.csv", "_wls.csv",
-               "_vox",
-               "_arc_well.csv", "_arc_hguid.csv", "_arc_bh.csv"))
+               "_voxler.csv",
+               "_archydro_well.csv", "_archydro_hguid.csv", "_archydro_bh.csv",
+               "_leapfrog_collars.csv", "_leapfrog_intervals.csv",
+               "_surfer.csv"))
     })
 
     # File headers
@@ -139,11 +174,16 @@ server_export_data <- function(id, wells) {
     output$archydro_f1 <- renderText(files()[5])
     output$archydro_f2 <- renderText(files()[6])
     output$archydro_f3 <- renderText(files()[7])
+    output$leapfrog_f1 <- renderText(files()[8])
+    output$leapfrog_f2 <- renderText(files()[9])
+    output$surfer_f1 <- renderText(files()[10])
 
-    # Export previews
+    # Export previews ---------------
     exp_strater <- reactive(wells_export(wells(), type = "strater", preview = TRUE))
     exp_voxler <- reactive(wells_export(wells(), type = "voxler", preview = TRUE))
     exp_archydro <- reactive(wells_export(wells(), type = "archydro", preview = TRUE))
+    exp_leapfrog <- reactive(wells_export(wells(), type = "leapfrog", preview = TRUE))
+    exp_surfer <- reactive(wells_export(wells(), type = "surfer", preview = TRUE))
 
     output$table_strater_f1 <- DT::renderDataTable({
       aq_dt(exp_strater()[[1]], minimal = TRUE)})
@@ -156,7 +196,12 @@ server_export_data <- function(id, wells) {
     output$table_archydro_f2 <- DT::renderDataTable(aq_dt(exp_archydro()[[2]], minimal = TRUE))
     output$table_archydro_f3 <- DT::renderDataTable(aq_dt(exp_archydro()[[3]], minimal = TRUE))
 
-    # Export files
+    output$table_leapfrog_f1 <- DT::renderDataTable(aq_dt(exp_leapfrog()[[1]], minimal = TRUE))
+    output$table_leapfrog_f2 <- DT::renderDataTable(aq_dt(exp_leapfrog()[[2]], minimal = TRUE))
+
+    output$table_surfer_f1 <- DT::renderDataTable(aq_dt(exp_surfer()[[1]], minimal = TRUE))
+
+    # Export files ---------------------
     output$feedback_strater <- feedback_output("strater")
     observe_export("strater")
 
@@ -165,6 +210,12 @@ server_export_data <- function(id, wells) {
 
     output$feedback_archydro <- feedback_output("archydro")
     observe_export("archydro")
+
+    output$feedback_leapfrog <- feedback_output("leapfrog")
+    observe_export("leapfrog")
+
+    output$feedback_surfer <- feedback_output("surfer")
+    observe_export("surfer")
 
   })
 
