8f8720dc0b473a1ca4dc4541abbdaf62fe98ed5f
Steffi LaZerte <steffi@steffi.ca>
2024-10-15 15:16:31 -0500

    Reorganize

--- R/mod_utils.R
+++ R/mod_utils.R
@@ -6,3 +6,30 @@ aq_tt <- function(trigger, ..., alt = "More information") {
     ...
   )
 }
+
+
+# File names - https://stackoverflow.com/a/56276939
+aq_dt <- function(data, filename = NULL, minimal = FALSE) {
+  if(minimal) {
+    opts <- list(dom = "tp")
+    ext <- list()
+  } else {
+    filename <- paste0(filename, "-", Sys.Date())
+    opts <- list(dom = 'Bfrtip',
+                 buttons = list(
+                   I('colvis'),
+                   list(extend = 'csv', title = filename),
+                   list(extend = 'excel', title = filename)
+                 ))
+    ext <- "Buttons"
+  }
+
+  data %>%
+    DT::datatable(
+      rownames = FALSE,
+      fillContainer = TRUE,
+      options = append(
+        list(pageLength = 14, scrollX = TRUE),
+        opts),
+      extensions = ext)
+}
--- R/utils.R
+++ R/utils.R
@@ -69,29 +69,3 @@ fix_leading_zero <- function(x) {
   stringr::str_replace_all(x, "(?<!\\d)(\\.\\d+)", "0\\1")
 }
 
-
-# File names - https://stackoverflow.com/a/56276939
-aq_dt <- function(data, filename = NULL, minimal = FALSE) {
-  if(minimal) {
-    opts <- list(dom = "tp")
-    ext <- list()
-  } else {
-    filename <- paste0(filename, "-", Sys.Date())
-    opts <- list(dom = 'Bfrtip',
-                 buttons = list(
-                   I('colvis'),
-                   list(extend = 'csv', title = filename),
-                   list(extend = 'excel', title = filename)
-                 ))
-    ext <- "Buttons"
-  }
-
-  data %>%
-    DT::datatable(
-      rownames = FALSE,
-      fillContainer = TRUE,
-      options = append(
-        list(pageLength = 14, scrollX = TRUE),
-        opts),
-      extensions = ext)
-}
